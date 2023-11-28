535acadbf63458416debe187161c8ea9f9817312
alifhasanshah <alifhasanshah@gmail.com>
2023-11-28 17:26:40 +0600

    user collection api created

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("tikidocsDB");
+    const userCollection = database.collection("users");
+    
+    // users collection api
+    app.post("/api/v1/users", async(req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send({result});
+    })
+    
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
